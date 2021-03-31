5f32b162dcaec6fc74ba8a5d979787d68bec53bf
irhamrizaldy <43570194+irhamrizaldy@users.noreply.github.com>
2021-03-31 14:16:52 +0700

    init commit

---
+++ includes/dbh.inc.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dBName = "rumahsakit";
+
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+
+if (!$conn) {
+    die("connection failed: " . mysqli_connect_error());
+} else {
+    echo 'Koneksi Database Berhasil';
+}
